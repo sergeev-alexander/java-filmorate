58f2dc236b392bdb8580118f46f66dcf812ab047
Sergeev-Alexander <alexander8168619@yandex.com>
2023-12-09 01:28:01 +0300

    commit 2.0

---
+++ src/test/java/ru/yandex/practicum/filmorate/storage/film/FilmStorageTest.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.storage.film;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmStorageTest {
+
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/storage/user/UserStorageTest.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.storage.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserStorageTest {
+
+}
